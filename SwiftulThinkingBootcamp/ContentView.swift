bbc7af66070992052bb86ffb02b8e90a0174adf2
Bruke Wondessen <bwondessen23@gmail.com>
2022-04-18 15:08:04 -0500

    Initial Commit

---
+++ SwiftulThinkingBootcamp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Bruke on 4/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftulThinkingBootcamp/SwiftulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulThinkingBootcampApp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Bruke on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
