8a7491f2044b206351951d1c93b2d2d3971b39ff
bwondessen <bwondessen23@gmail.com>
2024-11-03 23:17:11 -0600

    Initial Commit

---
+++ St. Louis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  St. Louis
+//
+//  Created by Bruke Wondessen on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ St. Louis/St__LouisApp.swift
@@ -0,0 +1,17 @@
+//
+//  St__LouisApp.swift
+//  St. Louis
+//
+//  Created by Bruke Wondessen on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct St__LouisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
